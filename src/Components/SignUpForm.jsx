a016c0ceed91475606da0c869245fc23e29601ec
Scott Cousins <scousins1024@yahoo.com>
2023-10-13 14:42:23 -0500

    complete first tab on workshop

---
+++ src/Components/SignUpForm.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+
+export default function SignUpForm() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        'https://fsa-jwt-practice.herokuapp.com/signup', {
+          method: "POST",
+          body: JSON.stringify({userName, password}),
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+    <h2>Sign up</h2>
+    {error && <p>{error}</p>}
+
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username: <input value={userName} onChange={(e) =>{setUserName(e.target.value)}}/>
+      </label>
+      <label>
+        Password: <input value={password} onChange={(e) =>{setPassword(e.target.value)}}/>
+      </label>
+      <button>Submit</button>
+    </form>
+    </>
+  )
+  }
+  
+  
