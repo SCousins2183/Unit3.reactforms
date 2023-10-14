07a54532dac0599dca70cbc0f0533cb3d546d58b
Scott Cousins <scousins1024@yahoo.com>
2023-10-14 08:49:12 -0500

    password input type to hide characters

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -32,7 +32,7 @@ export default function SignUpForm({ setToken }) {
         Username: <input value={userName} onChange={(e) =>{setUserName(e.target.value)}}/>
       </label>
       <label>
-        Password: <input value={password} onChange={(e) =>{setPassword(e.target.value)}}/>
+        Password: <input type="password" value={password} onChange={(e) =>{setPassword(e.target.value)}}/>
       </label>
       <button>Submit</button>
     </form>
